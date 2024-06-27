39c0bfca40251f347db1eae1f147a4653038f2eb
razikhan1999 <razikha1999@gmail.com>
2024-06-27 16:36:23 -0400

    feat(backend): add script to create mock users in the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+/**
+ * Asynchronously creates two users in the database using the Prisma client.
+ *
+ * @return {Promise<void>} A promise that resolves when both users have been created.
+ */
 async function main() {
   await prisma.user.create({
     data: mockUsers[Person.PersonA] as unknown as User,
