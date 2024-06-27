a1725308af7771001064fd222cea45ee8aaa778f
razikhan1999 <razikha1999@gmail.com>
2024-06-27 16:22:38 -0400

    feat(backend): add Company table and establish many-to-many relationship with Users

---
+++ prisma/migrations/20240626144628_user_company/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CompanyUser" (
+    "userId" INTEGER NOT NULL,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "CompanyUser_pkey" PRIMARY KEY ("userId","companyId")
+);
+
+-- AddForeignKey
+ALTER TABLE "CompanyUser" ADD CONSTRAINT "CompanyUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompanyUser" ADD CONSTRAINT "CompanyUser_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
